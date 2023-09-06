6f1c73f2aedbddbde27da2286f08eb3d1823cd24
hemmeljf <142923029+hemmeljf@users.noreply.github.com>
2023-09-06 18:20:46 -0400

    Delete bash/covid.sh

--- bash/covid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script downloads covid data and displays it
-
-DATA=$(curl https://api.covidtracking.com/v1/us/current.json)
-POSITIVE=$(echo $DATA | jq '.[0].positive')
-TODAY=$(date)
-
-echo "On $TODAY, there were $POSITIVE positive COVID cases"
