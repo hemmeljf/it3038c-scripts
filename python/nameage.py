da5776c1ab22d8896748e92733b7f00219cfae44
hemmeljf <142923029+hemmeljf@users.noreply.github.com>
2023-09-26 19:07:51 -0400

    Update nameage.py

--- python/nameage.py
+++ python/nameage.py
@@ -1,1 +1,9 @@
+print(‘What is your name?’) 
 
+myName = input(Jake Hemmelgarn) 
+
+print(‘Hello, ‘ + myName + ‘. That is a good name. How old are you?’) 
+
+myAge = input(22) 
+
+print(myAge+’? That’s funny, I'm only a few seconds old.’) 
