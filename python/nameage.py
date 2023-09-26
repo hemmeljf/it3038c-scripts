14224ef5999506f15ace1821857a30139daff905
hemmeljf <142923029+hemmeljf@users.noreply.github.com>
2023-09-26 19:50:00 -0400

    Update nameage.py

--- python/nameage.py
+++ python/nameage.py
@@ -17,3 +17,5 @@ print('%s? That is funny, I am only a %s seconds old' % (myAge, programAge))
 print('I wish I was %s years old' % (myAge * 2))
 
 time.sleep(3)
+
+print('I am tired. I go sleep sleep now.')
