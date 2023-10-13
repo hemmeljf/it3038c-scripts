d0271096e236a619131d90cdd78bc5773de02448
hemmeljf <142923029+hemmeljf@users.noreply.github.com>
2023-10-13 10:22:26 -0400

    Add files via upload

---
+++ powershell/machine.ps1
@@ -0,0 +1,20 @@
+﻿$Machines = 'localhost' 
+
+Foreach ($machine in $Machines)  
+
+{ 
+
+$pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 3).CounterSamples.CookedValue 
+
+    $sample = 1 
+
+    foreach ($p in $pt) { 
+
+        "Sample {2}: CPU is at {0}% on {1}" -f [int]$p, $machine, $sample | out-file C:\output.txt
+
+        $sample++ 
+
+    } 
+
+} 
+} 
